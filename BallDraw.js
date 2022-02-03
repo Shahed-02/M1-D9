6f3b4a151fbae1f542288b62025cf33594b142af
Shahed-02 <97530618+Shahed-02@users.noreply.github.com>
2022-02-03 15:53:59 +0000

    Add files via upload

---
+++ BallDraw.js
@@ -0,0 +1,13 @@
+function BallDraw (){
+    this.ballDrawnNumber = [];
+    this.ballDrawnNumber = []; 
+    
+    this.drawBall= () => {
+        let repeat= 0;
+        let ballLetter;
+        let ballNumber = 1 + Math.floor(Math.random()*76);
+        for (var index = 0 ; < 76; index++){
+            
+        }
+    }
+}
